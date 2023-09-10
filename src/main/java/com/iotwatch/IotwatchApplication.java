cac5318a47be347df750e2d216e61cf1940a139a
FP012 <fpt_12@hotmail.com>
2023-09-09 20:27:32 -0500

    Spring Boot project starting

---
+++ src/main/java/com/iotwatch/IotwatchApplication.java
@@ -0,0 +1,13 @@
+package com.iotwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotwatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iotwatch/IotwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iotwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
